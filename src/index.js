faef01e40b49371caf1553a7d12e29f9f94fca66
Jefferson <jeffarrubla@gmail.com>
2020-01-27 18:25:54 +0000

    JSX

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 
 const tasks = ['Take out the trash', 'Shovel the driveway','Walk the dog'];
 
-const element = React.createElement('ol', null, 	
-					tasks.map((task, index) => React.createElement('li', {key: index}, task)) 
-				);
+const element = 
+				<div>
+					<h1> Task List </h1>
+					<ol> 
+						{ tasks.map((task, index) => <li key={index}> {task} </li>) }	
+					</ol>
+				</div>
+				
 
 ReactDOM.render(element, document.getElementById('root'));
