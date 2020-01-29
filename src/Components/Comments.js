a4d09396f3f6c621242dcfedfe1bc5b3dfe6b7aa
Jefferson <jeffarrubla@gmail.com>
2020-01-29 22:36:03 +0000

    Loading comments per post

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -10,10 +10,11 @@ class Comments extends Component {
 	handleSubmit(event){
 		event.preventDefault()
 		const comment = event.target.elements.comment.value
-		this.props.addComment(comment)
+		this.props.addComment(comment, this.props.id)
 	}
 
 	render() {
+		console.log(this.props.comments)
 		return <div className="comment">
 					{
 						this.props.comments.map((comment, index) => {
