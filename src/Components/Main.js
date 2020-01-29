fe9d06eecf3288a1637a7a7de067a097e85a85cd
Jefferson <jeffarrubla@gmail.com>
2020-01-29 20:05:23 +0000

    Add post and improve title

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 import AddPhoto from './AddPhoto'
-import {Route} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
 import {removePost} from '../redux/actions'
 
 class Main extends Component {
@@ -13,18 +13,17 @@ class Main extends Component {
 	render() {
 		console.log(this.props)
 		return 	<div>
+					<h1>
+						<Link to="/"> Photowall </Link>
+					</h1>
 					<Route exact path="/" render={() => (						
 						<div>
-							<Title title={"Photowall"}/>	
 							<PhotoWall {...this.props}/>
 						</div>
 					)}/>
-					{/*<Route path="/addPhoto" render={({history}) => (
-						<AddPhoto onAddPhoto={(addedPost) => {
-							this.addPhoto(addedPost);
-							history.push('/');
-						}} />
-					)} />*/}
+					<Route path="/addPhoto" render={({history}) => (
+						<AddPhoto {...this.props} onHistory={history} />
+					)} />
 				</div>
 	}
 }
