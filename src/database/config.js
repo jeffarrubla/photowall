6c1e862c24bf40c83227e3bc945048faba40205b
Jefferson <jeffarrubla@gmail.com>
2020-01-30 15:42:23 +0000

    Setup firebase

---
+++ src/database/config.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCjiHbN2Gj6OpyB_WCpnbtpq_VtKSoKGt0",
+    authDomain: "photowall-7419b.firebaseapp.com",
+    databaseURL: "https://photowall-7419b.firebaseio.com",
+    projectId: "photowall-7419b",
+    storageBucket: "photowall-7419b.appspot.com",
+    messagingSenderId: "1032353571027",
+    appId: "1:1032353571027:web:438657a8bc2913b3763426",
+    measurementId: "G-8MR12YNJQV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  const database = firebase.database()
+
+  export {database}
