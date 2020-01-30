31705b82baa9204031b396137206ab963cf5533e
Jefferson <jeffarrubla@gmail.com>
2020-01-30 22:30:16 +0000

    Change title

--- src/database/config.js
+++ src/database/config.js
@@ -1,14 +1,7 @@
 import * as firebase from 'firebase'
 // Your web app's Firebase configuration
  var firebaseConfig = {
-    apiKey: "AIzaSyCjiHbN2Gj6OpyB_WCpnbtpq_VtKSoKGt0",
-    authDomain: "photowall-7419b.firebaseapp.com",
-    databaseURL: "https://photowall-7419b.firebaseio.com/",
-    projectId: "photowall-7419b",
-    storageBucket: "photowall-7419b.appspot.com",
-    messagingSenderId: "1032353571027",
-    appId: "1:1032353571027:web:03bfe5cfa4e1fee0763426",
-    measurementId: "G-4SCZBG92V2"
+    // Put your credentials here
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
