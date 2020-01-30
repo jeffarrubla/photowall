17488f2629ec464649f252402b9e8a5f35378c45
Jefferson <jeffarrubla@gmail.com>
2020-01-30 19:29:17 +0000

    config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ import * as firebase from 'firebase'
 
 // Your web app's Firebase configuration
   var firebaseConfig = {
-  
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
